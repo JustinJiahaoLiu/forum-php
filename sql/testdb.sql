cbba02a774f86cba263e94cbebc627760e124e20
Justin Liu <justinjiahaoliu@gmail.com>
2019-05-02 21:14:14 +1000

    Update SQL

--- sql/testdb.sql
+++ sql/testdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 12, 2019 at 02:30 AM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Generation Time: May 02, 2019 at 11:13 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `forums` (
 --
 
 INSERT INTO `forums` (`forum_id`, `forum_name`, `forum_Description`, `forum_image`) VALUES
-(1, 'Final Fantacy XIV', 'Your time is come', 'images/ff14_ico.png'),
+(1, 'Final Fantacy XIV', 'Your time has come', 'images/ff14_ico.png'),
 (2, 'Guild Wars 2', 'Go to Tyria, be a Tyrian', 'images/gw2_ico.png'),
 (3, 'World of Warcraft', 'Just play it!', 'images/wow_ico.png');
 
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `forum_posts` (
   `post_owner` varchar(150) DEFAULT NULL,
   `forum_id` int(11) NOT NULL,
   PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `forum_posts`
@@ -71,7 +71,8 @@ INSERT INTO `forum_posts` (`post_id`, `topic_id`, `post_text`, `post_create_time
 (1, 1, 'This is a test', '2019-03-29 10:39:42', 'test@test.com', 1),
 (2, 2, 'This is a test', '2019-03-29 10:40:50', 'test@test.com', 2),
 (3, 3, 'This is a test2', '2019-03-29 10:41:50', 'test2@test.com', 3),
-(4, 3, 'This an reply testing', '2019-03-29 11:54:02', 'test_rely@test.com', 3);
+(4, 3, 'This an reply testing', '2019-03-29 11:54:02', 'test_rely@test.com', 3),
+(5, 4, 'Testing Testing', '2019-04-12 21:29:59', 'FF14-fan@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -87,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `forum_topics` (
   `topic_owner` varchar(150) DEFAULT NULL,
   `forum_id` int(11) NOT NULL,
   PRIMARY KEY (`topic_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `forum_topics`
@@ -96,7 +97,8 @@ CREATE TABLE IF NOT EXISTS `forum_topics` (
 INSERT INTO `forum_topics` (`topic_id`, `topic_title`, `topic_create_time`, `topic_owner`, `forum_id`) VALUES
 (1, 'Test1', '2019-03-29 10:41:50', 'test1@test.com', 1),
 (2, 'Test2', '2019-03-29 10:41:50', 'test22@test.com', 2),
-(3, 'Test test2', '2019-03-29 10:41:50', 'test2@test.com', 3);
+(3, 'Test test2', '2019-03-29 10:41:50', 'test2@test.com', 3),
+(4, 'FF14 Forum Testing', '2019-04-12 21:29:59', 'FF14-fan@gmail.com', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
