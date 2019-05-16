3010f2c9686d7382d2f592237f51d777ea12aa3e
Justin Liu <justinjiahaoliu@gmail.com>
2019-05-17 07:39:05 +1000

    shop pages update

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -6,7 +6,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav mr-auto align-items-center">
             <li class="nav-item">
                 <a class="nav-link <?php echo isset($_GET['forum_id'])?"":"active"; ?>" href="index.php">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -19,10 +19,17 @@
             <li class="nav-item <?php echo isset($_GET['forum_id'])?($_GET['forum_id']=="3"?"active":""):""; ?>">
                 <a class="nav-link" href="topiclist.php?forum_id=3&forum_name=World of Warcraft">WoW</a>
             </li>
+            <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] == "/forum-php/seestore.php"?"active":"" ?>">
+                <a class="nav-link" href="seestore.php">Shop</a>
+            </li>
+            <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] == "/forum-php/showcart.php"?"active":"" ?>">
+                <a class="nav-link" href="showcart.php"><h3>&#128722;</h3></a>
+            </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
         </form>
+        
     </div>
 </nav>
