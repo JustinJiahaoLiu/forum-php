0adbad87f69fd00c8fdefc7c8ca1fb2bd204175c
W126 <W126@SYDRES.SydneyTAFE.edu.au>
2019-05-24 11:13:28 +1000

    Add stock control...

--- generateorder.php
+++ generateorder.php
@@ -14,16 +14,16 @@ $clean_order_tel = mysqli_real_escape_string($mysqli, $_POST['order_tel']);
 $clean_order_email = mysqli_real_escape_string($mysqli, $_POST['order_email']);
 $clean_item_total = mysqli_real_escape_string($mysqli, $_POST['item_total']);
 
-$store_orders = "INSERT INTO `store_orders`(`order_date`, `order_name`, `order_address`, `order_city`, `order_state`, `order_zip`, `order_tel`, `order_email`, `item_total`) VALUES (now(),'".$clean_order_name."','".$clean_order_address."','".$clean_order_city."','".$clean_order_state."','".$clean_order_zip."','".$clean_order_tel."','".$clean_order_email."','".$clean_item_total."')";
+$store_orders_sql = "INSERT INTO `store_orders`(`order_date`, `order_name`, `order_address`, `order_city`, `order_state`, `order_zip`, `order_tel`, `order_email`, `item_total`) VALUES (now(),'".$clean_order_name."','".$clean_order_address."','".$clean_order_city."','".$clean_order_state."','".$clean_order_zip."','".$clean_order_tel."','".$clean_order_email."','".$clean_item_total."')";
 
-$store_orders_res = mysqli_query($mysqli, $store_orders) or die(mysqli_error($mysqli));
+$store_orders_res = mysqli_query($mysqli, $store_orders_sql) or die(mysqli_error($mysqli));
 
 if($store_orders_res){
 	$last_id = mysqli_insert_id($mysqli);
 }
 
 //check for cart items based on user session id
-$get_cart_sql = "SELECT st.id, si.item_title, si.item_price,
+$get_cart_sql = "SELECT st.sel_item_id, si.item_title, si.item_price,
 st.sel_item_qty, st.sel_item_size, st.sel_item_color FROM
 store_shoppertrack AS st LEFT JOIN store_items AS si ON
 si.id = st.sel_item_id WHERE session_id =
@@ -35,18 +35,30 @@ or die(mysqli_error($mysqli));
 
 /*----------------------Store Orders Items---------------------*/
 while ($cart_info = mysqli_fetch_array($get_cart_res)) {
-	$id = mysqli_real_escape_string($mysqli, $cart_info['id']);
+	$id = mysqli_real_escape_string($mysqli, $cart_info['sel_item_id']);
 	
 	$item_price = mysqli_real_escape_string($mysqli, $cart_info['item_price']);
 	$item_qty = mysqli_real_escape_string($mysqli, $cart_info['sel_item_qty']);
 	$item_color = mysqli_real_escape_string($mysqli, $cart_info['sel_item_color']);
 	$item_size = mysqli_real_escape_string($mysqli, $cart_info['sel_item_size']);
 
-	$store_orders_items = "INSERT INTO `store_orders_items`(`order_id`, `sel_item_id`, `sel_item_qty`, `sel_item_size`, `sel_item_color`, `sel_item_price`) VALUES ('".$last_id."','".$id."','".$item_qty."','".$item_size."','".$item_color."','".$item_price."')";
+	$store_orders_items_sql = "INSERT INTO `store_orders_items`(`order_id`, `sel_item_id`, `sel_item_qty`, `sel_item_size`, `sel_item_color`, `sel_item_price`) VALUES ('".$last_id."','".$id."','".$item_qty."','".$item_size."','".$item_color."','".$item_price."')";
 
-	$store_orders_items_res = mysqli_query($mysqli, $store_orders_items)
+	$store_orders_items_res = mysqli_query($mysqli, $store_orders_items_sql)
+or die(mysqli_error($mysqli));
+	
+//---------Update Stock-------------
+	$stock_update_sql = " UPDATE `store_items` 
+		SET `store_items`.item_stock = `store_items`.item_stock - $item_qty
+		WHERE `store_items`.id = $id";
+	$stock_update_res = mysqli_query($mysqli, $stock_update_sql)
 or die(mysqli_error($mysqli));
 }
+
+//Remove shoppertrack log
+$delete_shoppertrack_sql = "DELETE FROM `store_shoppertrack` WHERE `store_shoppertrack`.session_id = '".$_COOKIE['PHPSESSID']."'";
+$delete_shoppertrack_res = mysqli_query($mysqli, $delete_shoppertrack_sql)
+or die(mysqli_error($mysqli));
 
 //clear cookie
 if (isset($_COOKIE['PHPSESSID'])) {
@@ -80,6 +92,7 @@ mysqli_close($mysqli);
 ?>
 <div class="container">
 	<h1>Thank you for shopping with us!</h1>
+	<a href="seestore.php"><button class="btn btn-success">Go Back to Shop</button></a>
 </div>
 </body>
 </html>
--- sql/testdb.sql
+++ sql/testdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 10, 2019 at 02:55 AM
+-- Generation Time: May 24, 2019 at 01:11 AM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `forum_posts` (
   `post_owner` varchar(150) DEFAULT NULL,
   `forum_id` int(11) NOT NULL,
   PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `forum_posts`
@@ -76,7 +76,12 @@ INSERT INTO `forum_posts` (`post_id`, `topic_id`, `post_text`, `post_create_time
 (6, 5, 'Now what?', '2019-05-03 11:36:21', 'long@abc.com', 1),
 (7, 5, 'This is not cool', '2019-05-03 12:45:51', 'troll_1@abc.com', 1),
 (8, 5, 'This is really bad', '2019-05-03 12:49:06', 'troll_2@cba.com', 1),
-(9, 5, 'This is a trolling train', '2019-05-03 12:51:18', 'troll_3@ccc.com', 1);
+(9, 5, 'This is a trolling train', '2019-05-03 12:51:18', 'troll_3@ccc.com', 1),
+(10, 6, 'We finally have season 5 online', '2019-05-17 10:33:52', 'GW2_fan@ab.com', 2),
+(11, 6, 'What the heck is season 5?', '2019-05-17 10:35:06', 'troll_NO1@trolling.com', 2),
+(12, 7, 'Testing', '2019-05-23 16:41:32', 'troll@gmail.com', 1),
+(13, 1, 'Testing', '2019-05-23 16:41:53', 'asdfa@a.c', 1),
+(14, 5, 'testing', '2019-05-24 09:56:02', 'testing@a.c', 1);
 
 -- --------------------------------------------------------
 
@@ -92,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `forum_topics` (
   `topic_owner` varchar(150) DEFAULT NULL,
   `forum_id` int(11) NOT NULL,
   PRIMARY KEY (`topic_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `forum_topics`
@@ -103,7 +108,9 @@ INSERT INTO `forum_topics` (`topic_id`, `topic_title`, `topic_create_time`, `top
 (2, 'Test2', '2019-03-29 10:41:50', 'test22@test.com', 2),
 (3, 'Test test2', '2019-03-29 10:41:50', 'test2@test.com', 3),
 (4, 'FF14 Forum Testing', '2019-04-12 21:29:59', 'FF14-fan@gmail.com', 1),
-(5, 'This is a testing for a very loooooooooooooooooooooooooooooooooooooooooong title', '2019-05-03 11:36:21', 'long@abc.com', 1);
+(5, 'This is a testing for a very loooooooooooooooooooooooooooooooooooooooooong title', '2019-05-03 11:36:21', 'long@abc.com', 1),
+(6, 'This is a really good day today', '2019-05-17 10:33:52', 'GW2_fan@ab.com', 2),
+(7, 'Testing', '2019-05-23 16:41:32', 'troll@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -152,14 +159,14 @@ CREATE TABLE IF NOT EXISTS `store_items` (
 --
 
 INSERT INTO `store_items` (`id`, `cat_id`, `item_title`, `item_price`, `item_desc`, `item_image`, `item_stock`) VALUES
-(1, 1, 'Baseball Hat', 12.00, 'Fancy, low-profile baseball hat.', 'images/baseballhat.gif', 99),
-(2, 1, 'Cowboy Hat', 52.00, '10 gallon variety', 'images/cowboyhat.gif', 99),
+(1, 1, 'Baseball Hat', 12.00, 'Fancy, low-profile baseball hat.', 'images/baseballhat.gif', 92),
+(2, 1, 'Cowboy Hat', 52.00, '10 gallon variety', 'images/cowboyhat.gif', 97),
 (3, 1, 'Top Hat', 102.00, 'Good for costumes.', 'images/tophat.gif', 99),
-(4, 2, 'Short-Sleeved T-Shirt', 12.00, '100% cotton, pre-shrunk.', 'images/sstshirt.gif', 99),
-(5, 2, 'Long-Sleeved T-Shirt', 15.00, 'Just like the short-sleeved shirt, with longer sleeves.', 'images/lstshirt.gif', 99),
+(4, 2, 'Short-Sleeved T-Shirt', 12.00, '100% cotton, pre-shrunk.', 'images/sstshirt.gif', 94),
+(5, 2, 'Long-Sleeved T-Shirt', 15.00, 'Just like the short-sleeved shirt, with longer sleeves.', 'images/lstshirt.gif', 97),
 (6, 2, 'Sweatshirt', 22.00, 'Heavy and warm.', 'images/sweatshirt.gif', 99),
 (7, 3, 'Jane\'s Self-Help Book', 12.00, 'Jane gives advice.', 'images/selfhelpbook.gif', 99),
-(8, 3, 'Generic Academic Book', 35.00, 'Some required reading for school, will put you to sleep.', 'images/boringbook.gif', 99),
+(8, 3, 'Generic Academic Book', 35.00, 'Some required reading for school, will put you to sleep.', 'images/boringbook.gif', 98),
 (9, 3, 'Chicago Manual of Style', 9.99, 'Good for copywriters.', 'images/chicagostyle.gif', 99);
 
 -- --------------------------------------------------------
@@ -225,8 +232,8 @@ CREATE TABLE IF NOT EXISTS `store_orders` (
   `order_name` varchar(100) DEFAULT NULL,
   `order_address` varchar(255) DEFAULT NULL,
   `order_city` varchar(50) DEFAULT NULL,
-  `order_state` char(2) DEFAULT NULL,
-  `order_zip` varchar(10) DEFAULT NULL,
+  `order_state` char(3) DEFAULT NULL,
+  `order_zip` varchar(4) DEFAULT NULL,
   `order_tel` varchar(25) DEFAULT NULL,
   `order_email` varchar(100) DEFAULT NULL,
   `item_total` float(6,2) DEFAULT NULL,
@@ -234,7 +241,24 @@ CREATE TABLE IF NOT EXISTS `store_orders` (
   `authorization` varchar(50) DEFAULT NULL,
   `status` enum('processed','pending') DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `store_orders`
+--
+
+INSERT INTO `store_orders` (`id`, `order_date`, `order_name`, `order_address`, `order_city`, `order_state`, `order_zip`, `order_tel`, `order_email`, `item_total`, `shipping_total`, `authorization`, `status`) VALUES
+(1, '2019-05-17 14:04:55', 'justin', '123 Pitt Street', 'Sydney', 'SA', '2000', '0400000000', 'justin.liu@gmailcom', 36.00, NULL, NULL, NULL),
+(2, '2019-05-17 14:15:25', 'justin', '123 Pitt Street', 'Sydney', 'SA', '2000', '0400000000', 'justin.liu@gmailcom', 36.00, NULL, NULL, NULL),
+(3, '2019-05-17 14:34:08', 'justin', '123 Pitt Street', 'Sydney', 'SA', '2000', '0400000000', 'justin.liu@gmailcom', 36.00, NULL, NULL, NULL),
+(4, '2019-05-17 14:41:55', 'joe', '222 Kent Str.', 'Sydney', 'NS', '2000', '040000000', 'joedoe@abc.com', 36.00, NULL, NULL, NULL),
+(6, '2019-05-17 14:48:04', 'Jane', '333 York Str', 'Sydney', 'NS', '2000', '0404001001', 'jane@a.c', 74.00, NULL, NULL, NULL),
+(7, '2019-05-23 16:40:55', 'jjksalsdf', 'jkljkladf', 'jkljkladf', 'jk', '2222', '000000000', 'adfda@a.c', 9.99, NULL, NULL, NULL),
+(8, '2019-05-24 09:59:01', 'justin', '123 Pitt ST.', 'Sydney', 'NS', '2000', '040000000', 'justin.liu@gmail.com', 47.00, NULL, NULL, NULL),
+(9, '2019-05-24 10:43:26', 'testing', 'testing', 'Sydney', 'NSW', '2001', '0400000000', 'testing@test.com', 15.00, NULL, NULL, NULL),
+(10, '2019-05-24 10:46:13', 'testing', 'testing', 'Sydney', 'NSW', '2001', '0400000000', 'testing@test.com', 15.00, NULL, NULL, NULL),
+(11, '2019-05-24 10:49:22', 'justin liu', '123 York Street', 'Sydney', 'NSW', '2000', '0400000001', 'justin.liu@gmail.com', 262.00, NULL, NULL, NULL),
+(12, '2019-05-24 11:08:58', 'justin', 'jkjljkad', 'jklj', 'nsw', '2000', '034000', 'jk@a.c', 36.00, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -252,7 +276,31 @@ CREATE TABLE IF NOT EXISTS `store_orders_items` (
   `sel_item_color` varchar(25) DEFAULT NULL,
   `sel_item_price` float(6,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `store_orders_items`
+--
+
+INSERT INTO `store_orders_items` (`id`, `order_id`, `sel_item_id`, `sel_item_qty`, `sel_item_size`, `sel_item_color`, `sel_item_price`) VALUES
+(1, 3, 14, 3, 'L', '', 12.00),
+(2, 4, 14, 3, 'L', '', 12.00),
+(3, 5, 15, 1, 'One Size Fits All', '', 102.00),
+(4, 5, 16, 1, 'S', '', 12.00),
+(5, 6, 17, 1, '', '', 35.00),
+(6, 6, 18, 1, '', '', 15.00),
+(7, 6, 19, 1, 'One Size Fits All', 'black', 12.00),
+(8, 6, 20, 1, 'M', '', 12.00),
+(9, 7, 21, 1, '', '', 9.99),
+(10, 8, 25, 1, '', '', 35.00),
+(11, 8, 26, 1, 'L', '', 12.00),
+(12, 10, 5, 1, '', '', 15.00),
+(13, 11, 2, 2, 'One Size Fits All', '', 52.00),
+(14, 11, 5, 1, '', '', 15.00),
+(15, 11, 4, 5, 'L', '', 12.00),
+(16, 11, 8, 1, '', '', 35.00),
+(17, 11, 1, 4, 'One Size Fits All', 'blue', 12.00),
+(18, 12, 1, 3, 'One Size Fits All', 'blue', 12.00);
 
 -- --------------------------------------------------------
 
@@ -270,18 +318,7 @@ CREATE TABLE IF NOT EXISTS `store_shoppertrack` (
   `sel_item_color` varchar(25) DEFAULT NULL,
   `date_added` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `store_shoppertrack`
---
-
-INSERT INTO `store_shoppertrack` (`id`, `session_id`, `sel_item_id`, `sel_item_qty`, `sel_item_size`, `sel_item_color`, `date_added`) VALUES
-(7, 'jslgnsr14o1r8hni509un80up1', 4, 1, 'L', '', '2019-05-10 12:24:00'),
-(8, 'jslgnsr14o1r8hni509un80up1', 4, 4, 'L', '', '2019-05-10 12:40:51'),
-(9, 'jslgnsr14o1r8hni509un80up1', 6, 2, '', '', '2019-05-10 12:41:00'),
-(10, 'jslgnsr14o1r8hni509un80up1', 1, 1, 'One Size Fits All', 'black', '2019-05-10 12:41:14'),
-(11, 'jslgnsr14o1r8hni509un80up1', 5, 1, '', '', '2019-05-10 12:45:47');
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
